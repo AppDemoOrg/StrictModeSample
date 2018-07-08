99904023c354ed3551893d8f3e3d38a9594b3dcb
huangweiqi <askviky2010@gmail.com>
2018-07-08 08:20:51 +0800

    初始化严苛模式

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# StrictModeSample
+
+## StrictModeSample
+
+### 参考文献
+1、[Android探索之旅 | StrictMode严苛模式](https://www.jianshu.com/p/113b9c54b5d1)
+2、
---
+++ app/src/main/java/com/abt/strictmode/SampleApp.java
@@ -0,0 +1,41 @@
+package com.abt.strictmode;
+
+import android.app.Application;
+import android.os.Build;
+import android.os.StrictMode;
+import android.support.annotation.RequiresApi;
+
+/**
+ * @描述： @SampleApp
+ * @作者： @黄卫旗
+ * @创建时间： @07/07/2018
+ */
+public class SampleApp extends Application {
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // 分别为MainThread和VirtualMachine设置StrictMode
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectDiskReads()
+                    .detectDiskWrites()
+                    .detectNetwork()
+                    .detectResourceMismatches()
+                    .detectCustomSlowCalls()
+                    .penaltyDeath()
+                    .build());
+
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects()
+                    .detectLeakedClosableObjects()
+                    .detectLeakedRegistrationObjects()
+                    .detectActivityLeaks()
+                    .penaltyDeath()
+                    .build());
+        }
+
+    }
+}
